7f4745707e9e4ff74b660bc9a51438687acec102
Archheretic <setervang@hotmail.com>
2017-02-04 17:12:19 +0100

    fix bug in dbconnection that prevented the server to swap databases based on environment variable

--- src/dbconnection.js
+++ src/dbconnection.js
@@ -1,13 +1,15 @@
 var mysql = require("mysql");
 var config = require("config");
 
+console.log(config.Database);
+console.log(config.util.getEnv('NODE_ENV'));
 var connection = mysql.createPool({
     // config files must be removed from git tracking before we add cloud solutions.
         connectionLimit : 100,
         host     : config.DBHost,
         user     : config.DBUser,
         password : config.DBPassword,
-        database : config.database,
+        database : config.Database,
         debug    :  false
 });
 
