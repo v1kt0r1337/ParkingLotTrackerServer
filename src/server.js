09ddba02a8d20eeb75c1822b225b0406360ba802
Archheretic <setervang@hotmail.com>
2017-02-19 14:55:11 +0100

    add CORS (Cross-Origin Resource Sharing)

--- src/server.js
+++ src/server.js
@@ -15,6 +15,12 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use('/api/v0/parkinglots', parkingLots);
 app.use('/api/v0/parkinglogs', parkingLogs);
 
