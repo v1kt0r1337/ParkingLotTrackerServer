83c64b125f0f6a922d6735ace4195a84a898a0bf
Archheretic <setervang@hotmail.com>
2017-02-26 12:08:45 +0100

    add token verification, that distinguish between admin and normal users.

---
+++ src/routes/authorize.js
@@ -0,0 +1,47 @@
+/**
+ * Created by archheretic on 26.02.17.
+ */
+let express = require('express');
+let router = express.Router();
+let config = require("config");
+let jwt = require("jsonwebtoken");
+// route middleware to verify a token
+
+let tokenVerification = {
+    verify: function(req, res, reqAdmin, next) {
+        // check header or url parameters or post parameters for token
+        let token = req.body.token || req.query.token || req.headers['x-access-token'];
+        // decode token
+        if (token) {
+            // verifies secret and checks exp
+            jwt.verify(token, config.secret, function(err, decoded) {
+                if (err) {
+                    return res.status(403).send({
+                        success: false,
+                        message: 'Failed to authenticate token.'
+                    });
+                } else {
+                    // if everything is good, save to request for use in other routes
+                    req.decoded = decoded;
+                    //console.log(req.decoded.admin);
+                    if (reqAdmin && !req.decoded.admin)
+                        return res.status(403).send({
+                            success: false,
+                            message: 'Failed to authenticate token, only users with admin rights can access this endpoint'
+                        });
+                    //console.log(req.decoded);
+                    next(req, res);
+                }
+            });
+        } else {
+            // if there is no token
+            // return an error
+            return res.status(403).send({
+                success: false,
+                message: 'No token provided.'
+            });
+        }
+    }
+};
+
+module.exports = tokenVerification;
